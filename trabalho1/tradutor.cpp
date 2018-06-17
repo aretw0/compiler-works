b60c4727d0fc6ade9ace8b78da593e8134d3543e
Unknown <aretw0@hotmail.com>
2018-06-17 04:18:38 -0300

    Tratando argumentos

--- trabalho1/tradutor.cpp
+++ trabalho1/tradutor.cpp
@@ -1,11 +1,20 @@
-#include <fstream>
 #include <locale.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+
 // #include "tradutor.h"
 
 using namespace std;
 
 
+static void mostrar_uso(string name)
+{
+    cerr << "Uso: " << name << " [ <opções> | SOURCE.exp ]" << endl
+    << "Opções:\n"
+    << "\t-h,--help\t\tMostra esta mensagem de ajuda\n"
+    << endl;
+}
 
 int main(int argc, char* argv[]) {
 	setlocale(LC_ALL, "Portuguese");	
@@ -15,13 +24,29 @@ int main(int argc, char* argv[]) {
 	}*/
 
  	// Checando o número de parâmetros
-    if (argc < 1) {
+    if (argc == 1) {
         // Conte ao usuário como rodar o programa
-        cerr << "Uso: " << argv[0] << " INPUTFILE.exp" << endl;
-      
+        mostrar_uso(argv[0]);
         return 1;
     }
-    // Print the user's name:
-    cout << argv[0] << "diz olá, " << argv[1] << "!" << endl;
+
+    if (argc > 1){
+        string args1 = argv[1]; 
+        if ((args1 == "--help") || (args1 == "-h")){
+            // Conte ao usuário como rodar o programa
+            mostrar_uso(argv[0]);
+            return 0;
+        }
+
+        // if (args1 == )
+    }
+    // Imprimir
+    // cout << argv[0] << " diz olá, " << argv[1] << "!" << endl;
+
+
+
+
+
+
     return 0;
 }
